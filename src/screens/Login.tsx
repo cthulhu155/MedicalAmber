2110c42236396a6c634e8821c721c017b2fd57ef
EliwtFdez <shadowkadizz@gmail.com>
2025-02-06 09:49:40 -0600

    Componetes no utilizados de version inicial eliminados

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,50 +1,58 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import { View,Image, Text, TouchableOpacity, StyleSheet, Alert, TextInput } from 'react-native';
 
-interface LoginProps {
-  setIsAuthenticated: (auth: boolean) => void;
-  navigation: any; // Puedes tipar esto según tus necesidades o usar StackNavigationProp
-}
+export default function Login({ navigation }) {
 
-export default function Login({ setIsAuthenticated, navigation }: LoginProps) {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = () => {
-    if (!username || !password) {
-      Alert.alert('Error', 'Por favor completa ambos campos.');
-      return;
-    }
-    // Lógica de autenticación simulada (reemplaza por tu lógica real)
-    if (username === 'admin' && password === 'admin') {
-      setIsAuthenticated(true);
+    if (email && password) {
+      Alert.alert('Inicio de sesión exitoso', 'Bienvenido de nuevo!');
+      navigation.navigate('Home');
     } else {
-      Alert.alert('Error', 'Usuario o contraseña incorrectos.');
+      Alert.alert('Error', 'Por favor, introduce tu correo y contraseña.');
     }
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Iniciar Sesión</Text>
+
+      <Image source={require('../../assets/images/MedicalAmber.png')} style={styles.logoImage}/>
+
+      <Text style={styles.title}>Iniciar sesión</Text>
+      <Text style={styles.subtitle}>Accede a tu cuenta de forma segura.</Text>
+
       <TextInput
-        placeholder="Usuario"
         style={styles.input}
-        value={username}
-        onChangeText={setUsername}
+        placeholder="Correo electrónico"
+        placeholderTextColor="#ccc"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
         autoCapitalize="none"
       />
+
       <TextInput
-        placeholder="Contraseña"
         style={styles.input}
-        secureTextEntry
+        placeholder="Contraseña"
+        placeholderTextColor="#ccc"
         value={password}
         onChangeText={setPassword}
+        secureTextEntry
+        autoCapitalize="none"
       />
-      <Button title="Iniciar Sesión" onPress={handleLogin} />
-      <Button
-        title="Registrarse"
-        onPress={() => navigation.navigate('Register')}
-      />
+
+      <TouchableOpacity style={styles.emailButton} onPress={handleLogin}>
+        <Text style={styles.emailButtonText}>Iniciar sesión</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.link} onPress={() => navigation.navigate('Register')}>
+        ¿Nuevo usuario? Regístrate
+      </Text>
+
+      <Text style={styles.help} onPress={() => Alert.alert('Ayuda', 'Para más información, contacta soporte.')}>¿Necesitas ayuda?</Text>
+
     </View>
   );
 }
@@ -53,20 +61,59 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    paddingHorizontal: 16,
-    backgroundColor: '#fff'
+    alignItems: 'center',
+    backgroundColor: '#1E2632',
+    padding: 20,
   },
   title: {
-    fontSize: 24,
-    marginBottom: 24,
+    fontSize: 22,
+    color: '#fff',
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 14,
+    color: '#ccc',
     textAlign: 'center',
+    marginBottom: 20,
   },
   input: {
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    marginBottom: 12,
-    paddingHorizontal: 8,
-    borderRadius: 4,
+    width: '100%',
+    backgroundColor: '#333',
+    color: '#fff',
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 15,
   },
+  emailButton: {
+    backgroundColor: '#FF9AA2',
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    marginBottom: 15,
+    width: '100%',
+    alignItems: 'center',
+  },
+  emailButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  link: {
+    color: '#FF9AA2',
+    marginTop: 10,
+  },
+  help: {
+    color: '#ccc',
+    marginTop: 20,
+  },
+  logoImage: {
+    width: 200,
+    height: 200,
+    borderRadius: 100, // Esto hace que la imagen sea circular
+    alignSelf: 'center',
+    marginTop: 10, // Ajusta este valor según sea necesario para posicionarla arriba
+    marginBottom: 20,
+  },
+
 });
