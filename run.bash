fefc5de23ab1f2fe9657bd3d69a7a34d4eee6636
EliwtFdez <shadowkadizz@gmail.com>
2025-02-06 21:39:35 -0600

    Revert "No se que valla a pasar; Backup antes de reset"
    This reverts commit 7ba61af6c4ab2b5ff009449824a118d265338214.

--- run.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Iniciando MedicalAmber con Docker..."
-docker build -t medicalamber .
-docker run -p 8081:8081 --rm medicalamber
-
--- src/types/Reminder.interface.tsx
+++
@@ -1,5 +0,0 @@
-interface Reminder {
-  id: string;
-  title: string;
-  time: string;
-}
