f19cd7aa81e29c51056f068ac8642b3508876c04
EliwtFdez <shadowkadizz@gmail.com>
2025-02-06 19:54:04 -0600

    botones de cambio de humbre mujer y espalda y en frente

--- src/screens/BodyHuman.tsx
+++ src/screens/BodyHuman.tsx
@@ -1,21 +1,55 @@
-import React from "react";
+import { StyleSheet, Switch, Text, View } from "react-native";
 import { useState } from "react";
-import { View, StyleSheet } from "react-native";
-import Body from "react-native-body-highlighter";
+import Body, { ExtendedBodyPart } from "react-native-body-highlighter";
+import React from "react";
 
 export default function BodyHuman() {
+  const [selectedBodyPart, setSelectedBodyPart] = useState<ExtendedBodyPart>({
+    slug: "biceps",
+    intensity: 2,
+    side: "right",
+  });
+  const [side, setSide] = useState<"front" | "back">("front");
+  const [gender, setGender] = useState<"male" | "female">("male");
+
+  const sideSwitch = () =>
+    setSide((previousState: string) => (previousState === "front" ? "back" : "front"));
+
+  const toggleGenderSwitch = () => {
+    setGender((previousState: string) =>
+      previousState === "male" ? "female" : "male"
+    );
+  };
+
   return (
     <View style={styles.container}>
       <Body
         data={[
           { slug: "chest", intensity: 1, side: "left" },
-          { slug: "biceps", intensity: 2 },
+          { slug: "biceps", intensity: 1 },
+          selectedBodyPart,
         ]}
-        gender="female"
-        side="front"
+        onBodyPartPress={(e, side) =>
+          setSelectedBodyPart({ slug: e.slug, intensity: 2, side })
+        }
+        gender={gender}
+        side={side}
         scale={1.7}
         border="#dfdfdf"
       />
+      <View style={styles.switchContainer}>
+        <View style={styles.switch}>
+          <Text>Side ({side})</Text>
+          <Switch onValueChange={sideSwitch} value={side === "front"} />
+        </View>
+        <View style={styles.switch}>
+          <Text>Gender ({gender})</Text>
+          <Switch
+            onValueChange={toggleGenderSwitch}
+            value={gender === "male"}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -24,6 +58,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  switchContainer: {
+    flexDirection: "row",
+    gap: 30,
+  },
+  switch: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
